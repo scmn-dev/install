13fa3722b9680949675f5bcc85d09397624ebdb8
abdfnx <abdfnabd@gmail.com>
2021-02-19 19:53:38 +0300

    mv installers to next level

--- install_osx.sh
+++ install_osx.sh
@@ -1,19 +1,50 @@
 #!/bin/bash
 
 # Installation
-# 1- check if curl command is exist
-# 2- check if wget command // /////
-# 3- check if brew command // /////
+# 1- rm old files
+# 2- check if curl command is exist
+# 3- some Linux platforms don't have git installed, so it's well checking is git command is exist
 
 GH_RAW_URL=https://raw.githubusercontent.com
+SM_DIR=~/sm
+smLocLD=/usr/local/bin
+_cgit=$GH_RAW_URL/secman-team/corgit/HEAD/cgit
+_verx$GH_RAW_URL/abdfnx/verx/HEAD/verx
+
+rmOldFiles() {
+    if [ -f $smLocLD/secman ]; then
+        sudo rm -rf $smLocLD/secman*
+        sudo rm -rf $smLocLD/verx*
+        sudo rm -rf $smLocLD/cgit*
+
+        if [ -d $SM_DIR ]; then
+            rm -rf ~/sm
+        fi
+    fi
+}
 
 # install deps
 echo "installing deps..."
-/bin/bash -c "$(curl -fsSL $GH_RAW_URL/secman-team/corgit/main/setup)"
-/bin/bash -c "$(curl -fsSL $GH_RAW_URL/abdfnx/verx/HEAD/install.sh)"
-sudo gem install bundler
+
+mkdir ~/sm
+
+curl -o $SM_DIR/cgit $_cgit
+curl -o $SM_DIR/verx $_verx
+
+# cgit, verx & secman-sync shortcuts
+cgit_shortcurt=$GH_RAW_URL/secman-team/secman/plugins/cgit
+verx_shortcurt=$GH_RAW_URL/secman-team/secman/plugins/verx
+secman_sync_shortcurt=$GH_RAW_URL/secman-team/secman/plugins/secman-sync
+backup_shortcurt=$GH_RAW_URL/secman-team/secman/plugins/backup
+
+curl -o $smLocLD/cgit $cgit_shortcurt
+curl -o $smLocLD/verx $verx_shortcurt
+curl -o $smLocLD/secman-sync $secman_sync_shortcurt
+curl -o $smLocLD/backup $backup_shortcurt
+
 cd ~
-wget $GH_RAW_URL/secman-team/secman/HEAD/Gemfile
+sudo gem install bundler
+curl $GH_RAW_URL/secman-team/secman/HEAD/Gemfile
 bundle install
 sudo rm -rf Gemfile*
 
@@ -22,76 +53,31 @@ v=$(verx secman-team/secman -l)
 smUrl=https://github.com/secman-team/secman/releases/download/$v/secman-osx
 sm_unUrl=$GH_RAW_URL/secman-team/secman/HEAD/packages/secman-un
 sm_syUrl=$GH_RAW_URL/secman-team/secman/HEAD/api/sync/secman-sync
-smLocLD=/usr/local/bin
+
+git clone https://github.com/secman-team/sm ~/sm
 
 successInstall() {
     echo "yesss, secman was installed successfully 😎, you can type secman --help"
 }
 
-installBrew() {
-    /bin/bash -c "$(curl -fsSL $GH_RAW_URL/Homebrew/install/HEAD/install.sh)"
-}
-
-installSecman_Tools() {    
+installSecman_Tools() {
     # secman
-    sudo wget -O $smLocLD $smUrl
+    sudo curl -o $smLocLD/secman $smUrl
 
     sudo chmod 755 $smLocLD/secman
 
     # secman-un
-    sudo wget -P $smLocLD $sm_unUrl
+    sudo curl -o $smLocLD $sm_unUrl
 
     sudo chmod 755 $smLocLD/secman-un
 
     # secman-sync
-    sudo wget -P $smLocLD $sm_syUrl
-
-    sudo chmod 755 $smLocLD/secman-sync
-}
-
-checkWget() {
-    if [ -x "$(command -v wget)" ]; then
-        installSecman_Tools
-    else
-        brew install wget
-
-        if [ -x "$(command -v wget)" ]; then
-            installSecman_Tools
-        fi
-    fi
-}
-
-gitAndBrew() {
-    if [ -x "$(command -v git)" ]; then
-        installBrew
-
-        if [ -x "$(command -v brew)" ]; then
-            checkWget
-        fi
-    fi
-}
-
-checkGit() {
-    xcode-select --install
-
-    gitAndBrew
+    sudo curl -o $SM_DIR/secman-sync $sm_syUrl
 }
 
 mainCheck() {
-    if [ -x "$(command -v brew)" ]; then
-        checkWget
-
-    else
-        if [ -x "$(command -v git)" ]; then
-            installBrew
-
-            if [ -x "$(command -v brew)" ]; then
-                checkWget
-            fi
-        else
-            checkGit
-        fi
-    fi
+    xcode-select --install
+    installSecman_Tools
 }
 
 if [ -x "$(command -v curl)" ]; then
