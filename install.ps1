51d026bb063ae04b478eb7f187a3872238564e0b
abdfnx <64256993+abdfnx@users.noreply.github.com>
2021-03-17 13:43:34 +0300

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -4,31 +4,42 @@ $loc = "$HOME\AppData\Local\secman"
 $smShUrl = "https://raw.githubusercontent.com/secman-team/tools/HEAD/sm.sh"
 $sm_winLoc = "$HOME\sm"
 
-curl $smShUrl -outfile sm.sh
+if (Test-Path -path $loc) {
+  Remove-Item $loc -Recurse -Force
+}
+
+if (Test-Path -path $sm_winLoc) {
+  Remove-Item $sm_winLoc -Recurse -Force
+}
+
+Invoke-WebRequest $smShUrl -outfile sm.sh
 
 $lv = bash sm.sh
 
-Write-Host "Installing secman..." -ForegroundColor DarkYellow
+Write-Host "Installing secman..." -ForegroundColor DarkCyan
 
-curl https://github.com/secman-team/secman/releases/download/$lv/secman_windows_${lv}_x64.zip -outfile secman_windows.zip
+Invoke-WebRequest https://github.com/secman-team/secman/releases/download/$lv/secman_windows_${lv}_x64.zip -outfile secman_windows.zip
 
 Expand-Archive secman_windows.zip
 
-mkdir $loc
+New-Item $loc
 
-mv secman_windows\bin $loc
+Move-Item secman_windows\bin $loc
 
-$env:Path += ";$HOME\AppData\Local\secman\bin"
+$env:Path += ";$HOME\$loc\bin"
 
 git clone https://github.com/secman-team/sm-win $sm_winLoc
 
-curl $smShUrl -outfile $sm_winLoc\sm.sh
+Invoke-WebRequest $smShUrl -outfile $sm_winLoc\sm.sh
 
 Write-Host "Installing ruby deps.."  -ForegroundColor DarkYellow
 
 gem install colorize optparse
 
-Remove-Item secman_windows* -Recurse -Force
-Remove-Item sm.sh
+Remove-Item secman_windows* sm.sh -Recurse -Force
 
-Write-Host "yessss, secman was installed successfully, run secman --help"  -ForegroundColor DarkGreen
+if (Test-Path -path $loc) {
+  Write-Host "Yessss, secman was installed successfully, run secman --help"  -ForegroundColor DarkGreen
+} else {
+  Write-Host "Download failed 😔"
+}
